df1800bbc4e57d494844a165f8edcf5b9f4d145a
Nathalie Benarroch <nbenarroch@jdlabs.co>
2021-07-22 19:47:32 -0400

    node express installed

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 // server drive thru closet
+
+const express = require('express');
+const app = express();
+
+
+
+
+
+
+
+app.listen(4000, '0.0.0.0', () => { console.log("Server running on port 4000") })
+
+
+
