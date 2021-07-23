800cde86a7cd994ef98749a76dceddb8dd4b874c
Nathalie Benarroch <nbenarroch@jdlabs.co>
2021-07-22 21:55:16 -0400

    sent message to front end

--- server.js
+++ server.js
@@ -3,7 +3,11 @@
 const express = require('express');
 const app = express();
 
-
+app.get('/', (req, res) => { 
+    console.log("tentative")                  
+    res.send({hello:"hello"});
+  });
+  
 
 
 
